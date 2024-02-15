10f2585d66f5ed314d978acc764bfa5a20eef20e
thlurte <thlurte@gmail.com>
2024-02-15 20:45:27 +0530

    postional encodings

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
- <h2 align="center">Siamese Neural Networks</h2>
+ <h2 align="center">Transformer Architecture</h2>
 <p align="center">A network architecture based on attention mechanism.</p>
 <br>
 
@@ -8,5 +8,22 @@
 
 ### Input Embeddings
 Here learned embeddings are used to convert the input tokens and output tokens to vectors of dimension $d_{model}$ and weights are multiplied by $\sqrt{d_{model}}$ .
+
+### Positional Encoding
+To provide some information about the relative or absolute position of the tokens in the sequence "positional encodings" are added to the input embeddings at the bottoms of the encoder and decoder stacks.
+
+The positional encodings have the same dimension $d_{model}$ as the embeddings, so that the two can be summed.
+
+> Denominator is calculated in log space.
+
+Sine and cosine functions of different frequencies are used:
+$$
+\begin{align}
+PE_{(pos,2i)}=\sin(pos/10000^{2i/d_{model}}) \\
+PE_{(pos,2i+1)}=\cos(pos/10000^{2i/d_{model}}) \\
+
+\end{align}
+$$
+In the input vector sine function is applied to the even positions and cos is applied to the odd positions.
 
 
--- model.py
+++ model.py
@@ -17,3 +17,34 @@ class InputEmbeddings(nn.Module):
 # -------------------------------------------------------------------------------- #
     
 
+
+# ---------------------------- Positional Encoding ------------------------------- #
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model:int, seq_len: int, dropout:float,*args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.d_model=d_model
+        self.seq_len=seq_len
+        self.dropout=nn.Dropout(dropout)
+
+        # Create a matrix of shape (seq_len,d_model)
+        pe=torch.zeros(seq_len,d_model)
+
+        # Create a vector of shape (seq_len,1)
+        position=torch.arange(0,seq_len,dtype=torch.float).unsqueeze(1)
+        div=torch.exp(torch.arange(0,d_model,2).float()*(np.log(10000.0)/d_model))
+        # Apply the sine function to even postion of the vector
+        pe[:,0::2]=torch.sin(position*div)
+        # Apply the cos function to odd postion of the vector
+        pe[:,1::2]=torch.cos(position*div)
+
+        # (1,seq_len,d_model)
+        pe=pe.unsqueeze(0)
+
+        self.register_buffer('pe',pe)
+
+
+    def forward(self,x):
+        x=x+(self.pe[:,:x.shape[1],:]).required_grad_(False)
+        return self.dropout(x)
+# -------------------------------------------------------------------------------- #
