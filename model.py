1199179089b0b5e017697583b95dfc121b70399c
thlurte <thlurte@gmail.com>
2024-02-17 14:50:30 +0530

    final block

--- model.py
+++ model.py
@@ -261,3 +261,51 @@ class ProjectionLayer(nn.Module):
         return torch.log_softmax(self.proj(x), dim=-1)
 
 # -------------------------------------------------------------------------------- #
+    
+
+
+
+# -------------------------------------------------------------------------------- #
+# ------------------------          Transformer          ------------------------- #
+# -------------------------------------------------------------------------------- #
+
+class Transformer(nn.Module):
+    def __init__(self
+                 , decoder:Decoder
+                 , encoder:Encoder
+                 , src_embed: InputEmbeddings
+                 , tgt_embed: InputEmbeddings
+                 , src_pos: PositionalEncoding
+                 , tgt_pos: PositionalEncoding
+                 , projection_layer: ProjectionLayer
+                 , *args
+                 , **kwargs
+                 
+                 ) -> None:
+        
+        super().__init__(*args, **kwargs)
+        self.encoder=encoder
+        self.decoder=decoder
+        self.src_embed=src_embed
+        self.tgt_embed=tgt_embed
+        self.src_pos=src_pos
+        self.tgt_pos=tgt_pos
+        self.projection_layer=projection_layer
+
+    def encode(self,src,src_mask):
+        src=self.src_embed(src)
+        src=self.src_pos(src)
+        return self.encoder(src,src_mask)
+    
+    def decode(self,encoder_output,src_mask,tgt,tgt_mask):
+        tgt=self.tgt_embed(tgt)
+        tgt=self.tgt_pos(tgt)
+        return self.decode(tgt, encoder_output, src_mask, tgt_mask)
+    
+
+    def project(self,x):
+        return self.projection_layer(x)
+    
+# -------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------- #
